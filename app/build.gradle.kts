9d7c10b5c75ff2696ee2e85a0e477b1df7399ddf
Riankai1073 <riancorci@gmail.com>
2024-01-08 18:59:57 -0800

    created img, txt, btn

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.and101_prework"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.and101_prework"
--- app/src/main/java/com/example/and101_prework/MainActivity.kt
+++ app/src/main/java/com/example/and101_prework/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.and101_prework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            Log.d("Main", "Button clicked!")
+            Toast.makeText(this, "Connected on LinkedIn!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
